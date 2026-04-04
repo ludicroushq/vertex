f1322e03df5b04f6f34efd1ff365dff2fb0cab39
Manthan Mallikarjun <me@nahtnam.com>
2026-04-03 17:09:30 -0700

    remove unused startup env validation

--- src/start.ts
+++ src/start.ts
@@ -2,12 +2,6 @@ import { createStart } from "@tanstack/react-start";
 import { authkitMiddleware } from "@workos/authkit-tanstack-react-start";
 import { serverEnv } from "./lib/config/server";
 
-void [
-  serverEnv.WORKOS_CLIENT_ID,
-  serverEnv.WORKOS_API_KEY,
-  serverEnv.WORKOS_COOKIE_PASSWORD,
-];
-
 export const startInstance = createStart(() => ({
   requestMiddleware: [
     authkitMiddleware({
