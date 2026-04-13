80d434570b2163b9f2102adbfe105ce80c5aea95
Manthan Mallikarjun <me@nahtnam.com>
2026-04-13 00:14:02 -0700

    redirect with portless

--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,11 @@
 import { createStart } from "@tanstack/react-start";
 import { authkitMiddleware } from "@workos/authkit-tanstack-react-start";
+import { appUrl } from "./lib/config";
 
 export const startInstance = createStart(() => ({
-  requestMiddleware: [authkitMiddleware()],
+  requestMiddleware: [
+    authkitMiddleware({
+      redirectUri: new URL("/api/auth/callback", appUrl).toString(),
+    }),
+  ],
 }));
