26b83956004b0212f0a415b8cb718e5884a76144
Manthan Mallikarjun <me@nahtnam.com>
2026-05-06 22:20:53 -0700

    always initialize posthog server client

--- src/lib/posthog/server.ts
+++ src/lib/posthog/server.ts
@@ -1,9 +1,7 @@
 import { PostHog } from "posthog-node";
 import { serverEnv } from "@/lib/config/server";
 
-export const posthog = serverEnv.POSTHOG_API_KEY
-  ? new PostHog(serverEnv.POSTHOG_API_KEY, {
-      flushAt: 1,
-      flushInterval: 0,
-    })
-  : undefined;
+export const posthog = new PostHog(serverEnv.POSTHOG_API_KEY ?? "", {
+  flushAt: 1,
+  flushInterval: 0,
+});
