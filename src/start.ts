7ae9458c5f6eaffd74c1bdafaaadef1410908c64
Manthan Mallikarjun <me@nahtnam.com>
2026-06-14 17:25:45 -0700

    Configure WorkOS redirect from public env

--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,11 @@
 import { createStart } from "@tanstack/react-start";
+import { configure } from "@workos/authkit-session";
 import { authkitMiddleware } from "@workos/authkit-tanstack-react-start";
 import { clientEnv } from "./lib/config/client";
+
+configure({
+  redirectUri: clientEnv.VITE_WORKOS_REDIRECT_URI,
+});
 
 export const startInstance = createStart(() => ({
   requestMiddleware: [
